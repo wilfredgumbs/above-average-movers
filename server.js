5f18342ee419644231b33a769637bd6c52ab7f18
BEAZERS BUSINESS LLC <wilfred.smithandsmith@gmail.com>
2019-05-12 22:07:34 -0400

    updated

--- server.js
+++ server.js
@@ -2,13 +2,9 @@ let express = require("express");
 let bodyParser = require("body-parser");
 let logger = require("morgan");
 
-let nodemailer = require('nodemailer');
-
-
-
+let nodemailer = require("nodemailer");
 
 // Require all models
-
 
 let PORT = process.env.PORT || 3000;
 
@@ -29,45 +25,45 @@ app.use(express.static("dist"));
 // By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
 // Connect to the Mongo DB
 
+app.post("/contact", function(req, res) {
+  console.log(req.body);
 
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: "aboveavg55@gmail.com",
+      pass: "Zebrastripes19"
+    }
+  });
 
+  let mailOptions = {
+    from: `"${req.body.name}"  <${req.body.email}>`,
+    to: "aboveavg55@gmail.com",
+    subject: "Quote request",
+    text: `from ${req.body.email}
+        \nclients name ${req.body.name}
+        \nphone number ${req.body.number || "N/A"}
+        \naddress ${req.body.address || "N/A"} ${req.body.address2 || ""}
+        \ncity: ${req.body.city || "N/A"}  state: ${req.body.state ||
+      "N/A"} zip: ${req.body.zip || "N/A"}
+        \nHow did they hear about you: ${req.body.reference || "N/A"}
+        \nSenior Moving: ${req.body.isSenior || "off"}
+        \n special instructions:  ${req.body.instructions || "N/A"}`
+  };
 
-
-app.post('/contact', function (req,res) {
-    console.log(req.body);
-
-
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
-        auth: {
-            user: "aboveavg55@gmail.com",
-            pass: "Zebrastripes19"
-        }
-    });
-
-    let mailOptions = {
-        from: `"${req.body.name}"  <${req.body.email}>`,
-        to: 'aboveavg55@gmail.com',
-        subject: '',
-        text: `from ${req.body.email} \nclients name ${req.body.name}  \n  ${req.body.instructions}`
-    };
-
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-            res.redirect('/');
-        }
-    });
-
-
-
-})
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+      res.redirect("/thankyou.html");
+    }
+  });
+});
 
 // Start the server
 app.listen(PORT, function() {
-    console.log("App running on port " + PORT + "!");
+  console.log("App running on port " + PORT + "!");
 });
